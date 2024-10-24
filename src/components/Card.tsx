0f65632a3f59237f790b7fd9b58afbd28659fa5c
Fiujy <momo.kerrouche31@gmail.com>
2024-10-24 09:05:48 +0200

    feat: gestion de la taille des images dans les cards de la page projects

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,6 +1,6 @@
 import { IProject } from "@/interfaces/IProjects"
 
-import picture  from "@/assets/images/Solaris.webp"
+import picture from "@/assets/images/Solaris.webp"
 
 interface Props {
     className?: string,
@@ -11,20 +11,15 @@ interface Props {
 const Card = ({ className = "", project, onClick }: Props) => {
     return (
         <div className={`card card-compact w-full shadow-xl rounded-2xl hover:scale-101 hover:shadow-2xl cursor-pointer ${className}`} onClick={onClick}>
-            <figure>
-                {project.imageName ? 
-                    <img
+            <div className="h-3/4">
+                <figure className="size-full">
+                    <img className="object-cover h-full w-auto max-w-full max-h-full"
                         // src={`@/assets/${project.imageName}.png`}
                         src={picture}
                         alt={project.title} />
-                    : 
-                    <img
-                        src={picture}
-                        alt={project.title}
-                    />    
-                    }
-            </figure>
-            <div className="card-body rounded-2xl bg-white">
+                </figure>
+            </div>
+            <div className="card-body rounded-2xl bg-white h-1/4">
                 <h2 className="card-title">{project.title} </h2>
                 <div className="card-actions">
                     <button className="text-primary hover:text-accent hover:underline" onClick={(e: React.MouseEvent<HTMLButtonElement>) => {
