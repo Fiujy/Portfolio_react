c7a6f897db1b771aa927d701d239780ccd412525
Fiujy <momo.kerrouche31@gmail.com>
2024-10-27 22:39:03 +0100

    feat: picture improved for more blank space

--- src/components/Picture.tsx
+++ src/components/Picture.tsx
@@ -5,7 +5,7 @@ export default function Picture() {
         <>
             <div className="bg-primary flex items-center h-full">
                 <div className="flex justify-center items-center">
-                    <img src={picture} alt="placeholder" className="w-4/5 h-4/5 rounded-xl shadow-2xl" />
+                    <img src={picture} alt="placeholder" className="w-3/4 h-4/5 rounded-xl shadow-2xl" />
                 </div>
             </div>
         </>
