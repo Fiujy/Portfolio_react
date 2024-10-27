931b4994dfd592756aa8333032b7c2d4acb2b803
Fiujy <momo.kerrouche31@gmail.com>
2024-10-27 19:29:27 +0100

    feat: ajouts de badges d'informations sur les technos dans la page d'accueil

--- src/components/Presentation.tsx
+++ src/components/Presentation.tsx
@@ -2,50 +2,14 @@ export default function Presentation() {
 
     return (
         <>
-            <div className='bg-white h-full'>
-                <div className='w-3/4 m-auto flex flex-col justify-center h-full gap-[5%]'>
+            <div className='bg-white h-full w-full'>
+                <div className='w-3/5 m-auto flex flex-col justify-center h-full gap-[5%] bg-transparent'>
                     <div className='space-y-3'>
                         <p className='text-6xl'>Mohamed Kerrouche</p>
                         <p className='text-4xl text-primary'>Developer, and even more.</p>
                     </div>
-                    <div className="carousel carousel-center bg-neutral rounded-box space-x-4 p-4 max-h-72">
-                        <div className="carousel-item">
-                            <img
-                                src="https://img.daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.webp"
-                                className="rounded-box" />
-                        </div>
-                        <div className="carousel-item">
-                            <img
-                                src="https://img.daisyui.com/images/stock/photo-1565098772267-60af42b81ef2.webp"
-                                className="rounded-box" />
-                        </div>
-                        <div className="carousel-item">
-                            <img
-                                src="https://img.daisyui.com/images/stock/photo-1572635148818-ef6fd45eb394.webp"
-                                className="rounded-box" />
-                        </div>
-                        <div className="carousel-item">
-                            <img
-                                src="https://img.daisyui.com/images/stock/photo-1494253109108-2e30c049369b.webp"
-                                className="rounded-box" />
-                        </div>
-                        <div className="carousel-item">
-                            <img
-                                src="https://img.daisyui.com/images/stock/photo-1550258987-190a2d41a8ba.webp"
-                                className="rounded-box" />
-                        </div>
-                        <div className="carousel-item">
-                            <img
-                                src="https://img.daisyui.com/images/stock/photo-1559181567-c3190ca9959b.webp"
-                                className="rounded-box" />
-                        </div>
-                        <div className="carousel-item">
-                            <img
-                                src="https://img.daisyui.com/images/stock/photo-1601004890684-d8cbf643f5f2.webp"
-                                className="rounded-box" />
-                        </div>
-                    </div>
-                    {/* <div className='flex'>
+                    {/* <div className="space-y-3">
+                        <div className='flex'>
                             <p className='font-bold mr-1'>Phone :</p>
                             <p className=''>000000</p>
                         </div>
@@ -56,10 +20,32 @@ export default function Presentation() {
                         <div className='flex'>
                             <p className='font-bold mr-1'>Address :</p>
                             <p className=''>Toulouse, France</p>
-                        </div> */}
-                    <div className='flex justify-center'> {/* Ajout de flex et justify-center */}
-                        <button className="btn text-white bg-primary hover:bg-accent w-1/4">
+                        </div>
+                    </div> */}
+                    <div className="space-y-3 w-full">
+                        <div className="flex flex-row gap-5">
+                            <div className="badge badge-accent badge-outline text-lg p-5">React</div>
+                            <div className="badge badge-accent badge-outline text-lg p-5">Symfony</div>
+                            <div className="badge badge-accent badge-outline text-lg p-5">Node.js</div>
+                            <div className="badge badge-accent badge-outline text-lg p-5">Nest.js</div>
+                        </div>
+                        <div className="flex flex-row gap-5">
+                            <div className="badge badge-accent badge-outline text-lg p-5">API Integration</div>
+                            <div className="badge badge-accent badge-outline text-lg p-5">Flutter</div>
+                            <div className="badge badge-accent badge-outline text-lg p-5">Tailwind</div>
+                        </div>
+                        <div className="flex flex-row gap-5">
+                            <div className="badge badge-accent badge-outline text-lg p-5">Docker</div>
+                            <div className="badge badge-accent badge-outline text-lg p-5">Postgres SQL</div>
+                            <div className="badge badge-accent badge-outline text-lg p-5">Git CI/CD</div>
+                        </div>
+                    </div>
+                    <div className='flex justify-center flex-row gap-3'>
+                        <button className="text-white btn bg-primary hover:bg-accent w-1/4">
                             My Work
+                        </button>
+                        <button className="text-accent border border-primary btn bg-white hover:bg-accent/20 w-1/4">
+                            Contact Me
                         </button>
                     </div>
                 </div>
